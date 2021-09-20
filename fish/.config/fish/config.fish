d5902dbf143bf0e1a83f36e8e8e9053d76700436
Kaisei Waki <me@kaisei.dev>
2021-09-20 23:18:03 +0900

    update: fish abbr

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,14 @@
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    abbr --add --global ls exa
+    abbr --add --global cat bat
+    abbr --add --global sudo doas
+    abbr --add --global vim nvim
+
+    abbr --add --global gs git status
+    abbr --add --global ga git add
+
     starship init fish | source
 end
